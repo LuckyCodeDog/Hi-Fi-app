aff3493184910f4fa9c100a4d5567effdb91e68e
LuckyCodeDog <118164873+LuckyCodeDog@users.noreply.github.com>
2023-08-07 11:38:54 +1200

    comit

---
+++ Hi_Fi/src/components/WifiCard.js
@@ -0,0 +1,29 @@
+import { Card, Button } from "@rneui/themed";
+import { View, StyleSheet, Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window")
+const CARD_HEIGHT = 220
+const CARD_WIDTH = width * 0.8
+const SPACE_FOR_CARD_INSET = CARD_WIDTH * 0.1 - 10
+
+
+const WifiCard = (props) => {
+    return (
+        <View style={styles.contianer}>
+            <Card>
+                <Card.Title>{props.cardTitle}</Card.Title>
+                <Card.Divider></Card.Divider>
+                <Button title='Connect Now' type="outline" loading={false}></Button>
+            </Card>
+        </View>
+    )
+}
+export default WifiCard
+const styles = StyleSheet.create({
+    contianer: {
+        width: CARD_WIDTH,
+        height: CARD_HEIGHT,
+        position:"relative",
+        bottom:-70
+    }
+})
--- Hi_Fi/src/components/WifiMapMarker.js
+++ Hi_Fi/src/components/WifiMapMarker.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {Marker} from 'react-native-maps'
-import {View,StyleSheet} from 'react-native'
+import {View,StyleSheet,Animated} from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 const WifiMapMarker =(props) =>{
+  useEffect(()=>{
+    // console.log(props.id)
+    // console.log(props.coordinate)
+  })
   return (
-    <Marker coordinate={props.coordinate}>
-      <View style={styles.view} > 
-      <Icon name='wifi' size={25} color={"white"} style={styles.icon} ></Icon>
-      </View>
+    <Marker  identifier ={props.id.toString()} coordinate={props.coordinate} onPress={props.onPressMarker}>
+      <Animated.View  style={[styles.view,{backgroundColor:props.color}]}  > 
+        <Icon name='wifi' size={25} color={"white"} style={styles.icon} ></Icon>
+      </Animated.View>
     </Marker>
   )
 }
@@ -20,7 +24,6 @@ const styles = StyleSheet.create({
     view:{
       width:40,
       height:40,
-      backgroundColor:"blue",
       borderRadius:50,
       alignContent:"center",
       justifyContent:'center'
