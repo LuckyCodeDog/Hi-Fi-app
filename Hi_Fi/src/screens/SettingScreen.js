d0a9111ad1bff1c93d5f69aa6a9d72d70fdaae85
Alex Liu <aleeex2333@gmail.com>
2023-09-12 21:08:19 +1200

    screen update
    scrrens update

--- Hi_Fi/src/screens/SettingScreen.js
+++ Hi_Fi/src/screens/SettingScreen.js
@@ -1,14 +1,57 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet, Switch } from 'react-native';
 
 const SettingScreen = () => {
   return (
-    <View>
-        <Text>
-            THIS IS SettingScreen
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Account</Text>
+      </View>
+      <View style={styles.settingItem}>
+        <Text style={styles.settingText}>Push notifications</Text>
+        <Switch />
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Default home screen</Text>
+      </View>
+      <View style={styles.settingItem}>
+        <Text style={styles.settingText}>Language</Text>
+        <Switch />
+      </View>
+      <View style={styles.settingItem}>
+        <Text style={styles.settingText}>Help and support</Text>
+        <Switch />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default SettingScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightpurple', // Use the desired light purple color
+  },
+  header: {
+    backgroundColor: 'white', // Use white as the header background color
+    padding: 16,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  settingItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+    backgroundColor: 'white',
+  },
+  settingText: {
+    fontSize: 18,
+  },
+});
+
+export default SettingScreen;
